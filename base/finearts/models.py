cc13460cf3c0f706ea84edc4c875a8746b86d785
ONLYDOWNDOGS <69099855+ONLYDOWNDOGS@users.noreply.github.com>
2021-09-24 18:28:47 -0600

    Fixed up model and homepage, added a start of a way to sort images in models.py. Also got pillow for image handling and added the current stable version to requirements.txt.

--- base/finearts/models.py
+++ base/finearts/models.py
@@ -5,13 +5,22 @@ STATUS = (
     (1,"Publish")
 )
 
+TECHNIQUE = (
+    (0,"Charcoal"),
+    (1,"Photoshop"),
+    (2,"Painting"),
+    (3,"Reel"),
+    (4,"Sketching"),
+    (5,"Illustrator")
+)
+
 class Portfolio(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     # To use if the images are already on the local machine, don't think it'll work for site owner
     # adding new files themselves
     # image = models.FilePathField(path="/img")
-    drawing = models.ImageField(upload_to='images/')
+    drawing = models.ImageField(upload_to='images')
     pub_date = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
